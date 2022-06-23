534aea5b69aa4d54edbc676f122ff9fd3332246b
Your Name <trongnhan7623@gmail.com>
2022-06-23 23:28:27 +0700

    update

---
+++ BigSchooll_PhanTrongNhan/ViewModels/CoursesViewModel.cs
@@ -0,0 +1,14 @@
+﻿using BigSchooll_PhanTrongNhan.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BigSchooll_PhanTrongNhan.ViewModels
+{
+    public class CoursesViewModel
+    {
+        public IEnumerable<Course> UpcommingCourses { get; set; }
+        public bool ShowAction { get; set; }
+    }
+}
